f59e95221260228fc31d983f74d1c7f0040e6a28
nereikia-linas <linas.nereikia@gmail.com>
2021-06-05 13:05:41 +0300

    Ok, padaryta su for loopu!

--- script.js
+++ script.js
@@ -1,11 +1,33 @@
 'use strict'
 
 var digits = [100, 21, 10.211, 300, 41, 21, -41, 0, 1, 27]
-const suma = (digits).reduce((partial_sum, z) => partial_sum + z,0);
-var minimum = Math.min.apply(Math, digits);
-var maximum = Math.max.apply(Math, digits);
-console.log('Duoti šie skaičiai: ' + digits);
-console.log('Duotų skaičių suma yra: ' + suma.toFixed(3));
-console.log('Duotų skaičių vidurkis yra: ' + (eval(digits.join('+'))/digits.length).toFixed(3));
-console.log('Didžiausias skaičius: ' + minimum);
-console.log('Mažiausias skaičius: ' + maximum);
+var sum = 0;
+var i;
+
+var stringLine = '';
+for (i = 0; i <= digits.length -1; i++) {
+  stringLine += '(' + digits[i] + ') ';
+}
+console.log('Duoti šie skaičiai: ' + stringLine);
+
+for (i=0; i < digits.length; i++) {
+  sum += digits[i];
+} console.log('Duotų skaičių suma yra: ' + sum.toFixed(3));
+
+var average = (sum / digits.length).toFixed(3);
+console.log('Duotų skaičių vidurkis yra: ' + average);
+
+var max = 0;
+for (i = 0; i < digits.length; i++) {
+  if (digits[i] > max) {
+    max = digits[i];
+  }
+} console.log('Didžiausias skaičius: ' + max);
+
+var min = max;
+for (i = 0; i < digits.length; i++) {
+  if (digits[i] < min)
+  {
+    min = digits[i];
+  }
+} console.log('Mažiausias skaičius: ' + min);
